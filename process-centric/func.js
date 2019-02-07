f9b7e900f3d85969c9f24fd063ce494e8de1df89
carpediem <mat.carpella@gmail.com>
2019-02-07 17:38:27 +0100

    added pool of fake coords

--- process-centric/func.js
+++ process-centric/func.js
@@ -1,7 +1,19 @@
+var usersFakeLocation = {}
+
 // fake coords
 const coords = {
-  lat: 53.4101422,
-  lng: -3.0240445
+	0: {
+		lat: 53.4101422,
+  	lng: -3.0240445		
+	},
+	1: {
+		lat: 52.477564,
+    lng: -2.0037166
+	},
+	2: {
+		lat: 50.913908,
+    lng: -1.4705332
+	}
 }
 
 exports.getDateTime = function () {
@@ -11,6 +23,15 @@ exports.getDateTime = function () {
 
 exports.getCoords = function (username) {
   console.log(usersLocation[username])
+
+  if (usersFakeLocation[username] === undefined) {
+  	let index = randCoords()
+  	usersFakeLocation[username] = [coords[index].lat, coords[index].lng].join(';')
+  }
+  return usersFakeLocation[username]
   // return usersLocation[username]
-  return coords.lat + ';' + coords.lng
+}
+
+function randCoords () {
+	return Math.floor(Math.random() * 3)
 }
