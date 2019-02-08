3df2074084c63bb412f0a79300d945a15123bb75
carpediem <mat.carpella@gmail.com>
2019-02-08 15:04:18 +0100

    added heroku deploy config conflict

--- web.js
+++ web.js
@@ -1,17 +1,19 @@
-var express = require('express')
-var packageInfo = require('./package.json')
-var bodyParser = require('body-parser')
+const express = require('express')
+const packageInfo = require('./package.json')
+const bodyParser = require('body-parser')
 
-var app = express()
+const app = express()
 app.use(bodyParser.json())
 
 app.get('/', function (req, res) {
   res.json({ version: packageInfo.version })
 })
 
-var server = app.listen(process.env.PORT, "0.0.0.0", function () {
-  var host = server.address().address
-  var port = server.address().port
+var host = ''
+var port = ''
+const server = app.listen(process.env.PORT, "0.0.0.0", function () {
+  host = server.address().address
+  port = server.address().port
 
   console.log('Web server started at http://%s:%s', host, port)
 })
